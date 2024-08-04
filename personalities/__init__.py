70b8d2a386e364f128102eba22539f6d793fdbbf
Rufus Pearce <161540961+ViciousSquid@users.noreply.github.com>
2024-08-04 20:27:52 +0100

    Delete personalities directory

--- personalities/__init__.py
+++
@@ -1,9 +0,0 @@
-# /personalities/__init__.py
-
-from .greedy_personality import GREEDY
-from .introvert_personality import INTROVERT
-from .lazy_personality import LAZY
-from .stubborn_personality import STUBBORN
-from .timid_personality import TIMID
-
-__all__ = ['GREEDY', 'INTROVERT', 'LAZY', 'STUBBORN', 'TIMID']
