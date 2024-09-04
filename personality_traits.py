255f50b7629fd2ab943d2ad935868aa9cb6ba9e8
Rufus Pearce <161540961+ViciousSquid@users.noreply.github.com>
2024-09-04 21:05:29 +0100

    Delete personality_traits.py

--- personality_traits.py
+++
@@ -1,8 +0,0 @@
-personality_traits = {}
-
-def register_personality(name, decision_function, attribute_modifiers):
-    personality_traits[name] = {
-        "decision_function": decision_function,
-        "attribute_modifiers": attribute_modifiers
-    }
-    return name
