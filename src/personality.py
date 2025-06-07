a8f9386a65d4c590b97f1bfb2434a6444ae9c2dd
Rufus Pearce <161540961+ViciousSquid@users.noreply.github.com>
2025-06-07 23:02:02 +0100

    2.2.1_stable  -- June 2025

--- src/personality.py
+++ src/personality.py
@@ -1,19 +1,6 @@
 from enum import Enum 
 
-from enum import Enum
-
 class Personality(Enum):
-    """Represents different personality traits.
-
-    Attributes:
-        TIMID: Timid personality.
-        ADVENTUROUS: Adventurous personality.
-        LAZY: Lazy personality.
-        ENERGETIC: Energetic personality.
-        INTROVERT: Introverted personality.
-        GREEDY: Greedy personality.
-        STUBBORN: Stubborn personality.
-    """
     TIMID = "timid"
     ADVENTUROUS = "adventurous"
     LAZY = "lazy"
